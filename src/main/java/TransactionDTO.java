9e836bc699c6efe7a22275d588fe5026e1c8c202
Tien <Hoang>
2013-06-17 09:44:21 +0700

    Step 7: green

--- src/main/java/TransactionDTO.java
+++ src/main/java/TransactionDTO.java
@@ -6,8 +6,68 @@
  * To change this template use File | Settings | File Templates.
  */
 public class TransactionDTO {
+    private String accountNumber;
+    private double amount;
+    private long timeStamp;
+    private String log;
 
     public TransactionDTO(String accountNumber, double amount, long timeStamp, String log) {
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+        this.timeStamp = timeStamp;
+        this.log = log;
+    }
 
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getLog() {
+        return log;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public void setLog(String log) {
+        this.log = log;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TransactionDTO that = (TransactionDTO) o;
+
+        if (Double.compare(that.amount, amount) != 0) return false;
+        if (timeStamp != that.timeStamp) return false;
+        if (accountNumber != null ? !accountNumber.equals(that.accountNumber) : that.accountNumber != null)
+            return false;
+        if (log != null ? !log.equals(that.log) : that.log != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
     }
 }
